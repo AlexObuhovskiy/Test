f8a392c275ceb23106450b1ece8a954f11dd16db
Oleksandr Obukhivskyi <alexobuhovskiy@gmail.com>
2020-04-20 12:16:45 +0300

    removed redundant code

--- Test/ClientApp/src/app/app.component.ts
+++ Test/ClientApp/src/app/app.component.ts
@@ -28,47 +28,4 @@ export class AppComponent implements OnInit {
         this.dataService.calculate(this.calculatorModel)
             .subscribe((result: any) => this.calculatorResult = result);
     }
-
-
-
-    // product: Product = new Product();
-    // products: Product[];
-    // tableMode: boolean = true;
-
-    // constructor(private dataService: DataService) { }
-
-    // ngOnInit() {
-    //     this.loadProducts();
-    // }
-
-    // loadProducts() {
-    //     this.dataService.getProducts()
-    //         .subscribe((data: Product[]) => this.products = data);
-    // }
-
-    // save() {
-    //     if (this.product.id == null) {
-    //         this.dataService.createProduct(this.product)
-    //             .subscribe((data: Product) => this.products.push(data));
-    //     } else {
-    //         this.dataService.updateProduct(this.product)
-    //             .subscribe(data => this.loadProducts());
-    //     }
-    //     this.cancel();
-    // }
-    // editProduct(p: Product) {
-    //     this.product = p;
-    // }
-    // cancel() {
-    //     this.product = new Product();
-    //     this.tableMode = true;
-    // }
-    // delete(p: Product) {
-    //     this.dataService.deleteProduct(p.id)
-    //         .subscribe(data => this.loadProducts());
-    // }
-    // add() {
-    //     this.cancel();
-    //     this.tableMode = false;
-    // }
 }
